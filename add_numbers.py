3e09f98ddd34bf8c8cbc9d24548cab1799bf59db
saidavanam <saidavanam183@gmail.com>
2025-06-27 12:24:17 +0530

    second change

--- add_numbers.py
+++ add_numbers.py
@@ -1,7 +1,8 @@
-def add(x,y):
-    return x + y
+def add(x,y,z):
+    return x + y + z
 
 x = int(input("Enter the number 1 "))
 y = int(input("Enter the number 2"))
+z = int(input("Enter the number 3"))
 
-print(add(x,y))
+print(add(x,y,z))
